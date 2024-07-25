ee73bb78d4cee9cfe6e11c689cf3dbb8a26b0f58
YuuyaRin <feng93115@126.com>
2024-07-25 19:38:43 +0800

    配置测试

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,11 +3,17 @@ export default defineConfig({
   title: 'Yuuya-UI',
   description: 'A UI library based on Vue 3',
   lang: 'zh-CN',
-  base: '/yuuya-ui/',
+  base: '/',
   lastUpdated: true,
   themeConfig: {
     logo: '/ui-logo.svg',
     siteTitle: 'Yuuya-UI文档',
-    outline: 3
+    outline: 3,
+    socialLinks: [
+      {
+        icon: 'github',
+        link: 'https://github.com/YuuyaRin/yuuya-ui'
+      }
+    ]
   }
 })
