6924f73673bddccce63336b38e33a97c8712091a
YuuyaRin <feng93115@126.com>
2024-10-31 11:54:34 +0800

    test:vitest测试demo

--- example.test.ts
+++ example.test.ts
@@ -1,7 +1,10 @@
 // 执行 npx vitest example
 
-import { expect, test, describe, vi } from 'vitest'
-import { testFn } from './utils'
+import { expect, test, describe, vi, Mocked } from 'vitest'
+import { testFn, request } from './utils'
+import axios from 'axios' // 引入 axios 模块
+vi.mock('axios') // 模拟 axios 模块
+const mockAxios = axios as Mocked<typeof axios> // 类型断言 将 axios 模块模拟为 Mocked 类型
 // test('test common matcher', () => {
 //   const name = 'yuuya'
 //   expect(name).toBe('yuuya')
@@ -31,15 +34,26 @@ describe('functions', () => {
     expect(callback).toHaveBeenCalled() // 是否调用过
     expect(callback).toHaveBeenCalledWith(12) // 是否调用过并传入了预期的参数 -> 12
   })
-  // 监控函数 6:47
+  // 监控函数
   test('spy on method', () => {
     const obj = {
       getName: () => 1
     }
+    // spyOn 方法 用于监控对象的方法是
     // 监控 getName 方法是否被调用
     const spy = vi.spyOn(obj, 'getName')
     obj.getName()
     // 验证 getName 方法是否被调用
     expect(spy).toHaveBeenCalled()
+    obj.getName()
+    // 验证 getName 方法是否被调用了两次
+    expect(spy).toHaveBeenCalledTimes(2)
+  })
+  // 模拟第三方模块
+  test('mock third party module', async () => {
+    // mockAxios.get.mockImplementation(() => Promise.resolve({ data: 123 })) // 模拟 axios.get 方法返回值为 123
+    mockAxios.get.mockResolvedValue({ data: 123 }) // 模拟 axios.get 方法返回值为 123 (两种写法都可以)
+    const result = await request() // 调用 request 方法
+    expect(result).toBe(123) // 验证返回值为 123
   })
 })
