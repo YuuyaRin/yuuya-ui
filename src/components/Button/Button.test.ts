d32b21857994afbeba73d597d9728931aa3b3611
YuuyaRin <feng93115@126.com>
2024-11-01 17:18:13 +0800

    test:[Button] basic button,disabled button 属性测试

--- src/components/Button/Button.test.ts
+++ src/components/Button/Button.test.ts
@@ -1,4 +1,4 @@
-import { describe, test } from 'vitest'
+import { describe, test, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import Button from './Button.vue'
 
@@ -14,6 +14,32 @@ describe('Button.vue', () => {
       }
     })
     console.log(wrapper.html()) // 打印组件的html
-    // expect(wrapper.find('.btn-primary').exists()).toBe(true)
+
+    expect(wrapper.classes()).toContain('yuuya-button--primary') // 断言组件的class是否包含yuuya-button--primary
+
+    // get方法获取组件中的元素
+    expect(wrapper.get('button').text()).toBe('button') // 断言组件Slot中的text是否为button
+
+    // events
+    wrapper.get('button').trigger('click') // 触发click事件
+    console.log('事件:', wrapper.emitted()) // 打印组件的emit事件
+    expect(wrapper.emitted()).toHaveProperty('click') // 断言组件的emit事件是否包含click
+  })
+  test('disabled button', () => {
+    const wrapper = mount(Button, {
+      props: {
+        disabled: true
+      },
+      slots: {
+        default: 'disabled'
+      }
+    })
+    expect(wrapper.attributes('disabled')).toBeDefined() // 断言组件的button是否包含disabled属性
+    expect(wrapper.find('button').element.disabled).toBeDefined() // 断言组件的button是否为disabled,element为组件的根元素(原生属性)
+
+    // events
+    wrapper.get('button').trigger('click') // 触发click事件
+    console.log('事件:', wrapper.emitted()) // 打印组件的emit事件
+    expect(wrapper.emitted()).not.toHaveProperty('click') // 断言组件的emit事件是否包含click
   })
 })
