9bd896da4081cdc68b3e54348f72275f7162d1c8
YuuyaRin <feng93115@126.com>
2024-12-13 16:13:18 +0800

    test:[Collapse] 子组件数量、文本、内容展示切换、点击行为、禁用行为测试

---
+++ src/components/Collapse/Collapse.test.tsx
@@ -0,0 +1,61 @@
+import { describe, expect, test } from 'vitest'
+import { h } from 'vue'
+import { mount } from '@vue/test-utils'
+import Collapse from './Collapse.vue'
+import CollapseItem from './CollapseItem.vue'
+describe('Collapse.vue', () => {
+  test('basic collapse', async () => {
+    const wrapper = mount(
+      () => (
+        <Collapse modelValue={['a']}>
+          <CollapseItem name="a" title="title a">
+            content a
+          </CollapseItem>
+          <CollapseItem name="b" title="title b">
+            content b
+          </CollapseItem>
+          <CollapseItem name="c" title="titble c" disabled>
+            content c
+          </CollapseItem>
+        </Collapse>
+      ),
+      {
+        global: {
+          stubs: ['Icon']
+        },
+        attachTo: document.body // 挂载到 document.body 下, 用于处理操作后样式不更新的Bug
+      }
+    )
+    const headers = wrapper.findAll('.yuuya-collapse-item__header')
+    const contents = wrapper.findAll('.yuuya-collapse-item__wrapper')
+
+    // 子组件 CollapseItem 数量
+    expect(headers.length).toBe(3)
+    expect(contents.length).toBe(3)
+
+    // 文本
+    const firstHeader = headers[0]
+    const secondHeader = headers[1]
+    expect(firstHeader.text()).toBe('title a')
+
+    // 内容
+    const firstContent = contents[0]
+    const secondContent = contents[1]
+    const disabledContent = contents[2]
+    expect(firstContent.isVisible()).toBeTruthy()
+    expect(secondContent.isVisible()).toBeFalsy()
+    expect(firstContent.text()).toBe('content a')
+
+    // 行为
+    await firstHeader.trigger('click')
+    expect(firstContent.isVisible()).toBeFalsy()
+    await secondHeader.trigger('click')
+    expect(secondContent.isVisible()).toBeTruthy()
+
+    // disabled
+    const disabledHeader = headers[2]
+    expect(disabledHeader.classes()).toContain('is-disabled')
+    await disabledHeader.trigger('click')
+    expect(disabledContent.isVisible()).toBeFalsy()
+  })
+})
